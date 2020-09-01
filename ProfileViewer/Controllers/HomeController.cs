a43a2ecafbf04ec431dcd9ceece65982213a411e
shahed48 <shahjahan.cseku11@gmail.com>
2020-09-01 22:53:01 +0600

    UI project

--- ProfileViewer/Controllers/HomeController.cs
+++ ProfileViewer/Controllers/HomeController.cs
@@ -1,28 +1,12 @@
-﻿using Core.Service;
-using Core.ViewModel;
-using Microsoft.AspNet.Identity;
-using ProfileViewer.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace ProfileViewer.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly UserService userService;
-        private readonly WorksService worksService;
-        public HomeController()
-        {
-            userService = new UserService();
-            worksService = new WorksService();
-        }
-
         public ActionResult Index()
         {
-            if (!User.Identity.IsAuthenticated)
+            if (Session["token"] == null)
                 return RedirectToAction("Login", "Account");
 
             return View();
@@ -31,7 +15,7 @@ namespace ProfileViewer.Controllers
         [HttpGet]
         public ActionResult Dashboard()
         {
-            if (!User.Identity.IsAuthenticated)
+            if (Session["token"] == null)
                 return RedirectToAction("Login", "Account");
 
             return PartialView("_dashboard");
@@ -40,33 +24,28 @@ namespace ProfileViewer.Controllers
         [HttpGet]
         public ActionResult UserProfile()
         {
-            if (!User.Identity.IsAuthenticated)
+            if (Session["token"] == null)
                 return RedirectToAction("Login", "Account");
 
-            return PartialView("_profile", userService.GetUserProfileByEmail(User.Identity.GetUserName()));
+            return PartialView("_profile");
         }
 
         [HttpPost]
-        public ActionResult UpdateProfile(ProfileViewModel model)
+        public ActionResult UpdateProfile()
         {
-            return Json(userService.UpdateProfile(model), JsonRequestBehavior.AllowGet);
+            if (Session["token"] == null)
+                return RedirectToAction("Login", "Account");
+
+            return Json("", JsonRequestBehavior.AllowGet);
         }
 
         [HttpGet]
         public ActionResult Works()
         {
-            if (!User.Identity.IsAuthenticated)
+            if (Session["token"] == null)
                 return RedirectToAction("Login", "Account");
+
             return PartialView("_works");
-        }
-
-        [HttpPost]
-        public ActionResult GetWorksOfLoggedUser()
-        {
-            if(!User.Identity.IsAuthenticated)
-                return RedirectToAction("Login", "Account");
-
-            return Json(worksService.GetTaskForUser(User.Identity.GetUserName()), JsonRequestBehavior.AllowGet);
         }
     }
 }
--- ProfileViewer/Scripts/app/app.js
+++ ProfileViewer/Scripts/app/app.js
@@ -1,11 +1,55 @@
 ﻿
+function login() {
+    var userName = document.getElementById('emailAddress').value;
+    var password = document.getElementById('password').value;
+    $(".loader").show();
+    $.ajax({
+        type: 'POST',
+        url: development_api + 'account/login?pUserName=' + userName + '&pPassword=' + password,
+        success: function (resp) {
+            localStorage.setItem('token', 'Bearer ' + resp);
+            $.ajax({
+                type: 'POST',
+                url: '/Account/SetToken',
+                data: { token: localStorage.getItem('token') },
+                success: function (resp) {
+                    window.location.href = '/Home/Index';
+                },
+                error: function (resp) {
+                    $(".loader").hide();
+                    alert("Something went wrong");
+                }
+            });
+        },
+        error: function (resp) {
+            $(".loader").hide();
+            alert("incorrect username or password");
+        }
+    });
+}
+
+function logout() {
+
+    $.ajax({
+        type: 'POST',
+        url: development_api + 'account/logout',
+        success: function (resp) {
+            localStorage.clear();
+            window.location.href = '/Account/Logout';
+        },
+        error: function (resp) {
+            console.log(resp);
+        }
+    });
+}
+
 function UpdateSelect(e, selection) {
     $(".nav-anchor").each(function (index, e) {
         if ($(this).hasClass('active')) {
             $(this).removeClass('active').removeClass('arrow');
         }
     });
-    $(e.target).addClass('active').addClass('arrow');
+    $(e).addClass('active').addClass('arrow');
 
     var action_name = 'Dashboard';
     if (selection === 'profile') { action_name = 'UserProfile'; }
@@ -42,6 +86,15 @@ function LoadViews(action_name) {
 $("ul#asideNav li:first-child a").click();
 LoadViews('Dashboard');
 
-function Logout() {
-    document.getElementById('logoutForm').submit();
+function ToggleNavigation(e) {
+    if ($('.main-content').hasClass('main-content-max')) {
+        $('.main-content').removeClass('main-content-max');
+        $('.sidebar').removeClass('sidebar-min');
+        $('.nav-label').removeClass('d-none');
+    }
+    else {
+        $('.main-content').addClass('main-content-max');
+        $('.sidebar').addClass('sidebar-min');
+        $('.nav-label').addClass('d-none');
+    }
 }
